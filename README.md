1e102f89f120236846bb3c2346f3133c6d32f302
Harsh Dagar <amandagar000@gmail.com>
2023-09-15 02:12:03 +0530

    Initial release

---
+++ README.md
@@ -0,0 +1,31 @@
+# envdot README
+
+Get auto-complete suggestions for the variables from your `.env` or `.env.local` files in workspace
+
+## Features
+
+> ### Supports
+
+- Languages: `JavaScript`,`TypeScript`
+- Formats: `js`,`jsx`,`ts`,`tsx`
+  > ### Auto-Completion Suggestions
+- Provides auto-completion for environment variables by typing: `env.`
+- Supports [Vite](https://vitejs.dev/)'s special import object: `import.meta.env.`
+
+For example if there is an image subfolder under your extension project workspace:
+
+[Auto-Complete Suggestions](src/assets/Recording.gif)
+
+## Requirements
+
+VS Code: 1.82 or earlier
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of the extension.
+
+---
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,85 @@
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.languages.registerCompletionItemProvider(
+      ["javascript", "typescript"],
+      new EnvCompletionItemProvider(),
+      "."
+    )
+  );
+}
+type EnvVarItem = {
+  name: string;
+  value: string;
+  file: string;
+};
+class EnvCompletionItemProvider implements vscode.CompletionItemProvider {
+  provideCompletionItems(
+    document: vscode.TextDocument,
+    position: vscode.Position,
+    token: vscode.CancellationToken
+  ): Thenable<vscode.CompletionItem[]> {
+    return new Promise((resolve, reject) => {
+      let line = document.lineAt(position).text;
+      if (line.match(/env\./)) {
+        let envVariables: EnvVarItem[] = [];
+        const workspaceFolder = vscode.workspace.workspaceFolders; //TODO: This should be selectable in case of multiple env files
+        if (workspaceFolder) {
+          workspaceFolder.forEach((folder) => {
+            this.getEnvFilesInDir(folder.uri.fsPath, envVariables);
+          });
+        }
+        let uniqueEnvVariables = [...new Set(envVariables)];
+        resolve(
+          uniqueEnvVariables.map(({ name, value, file }, index) => {
+            const cItem = new vscode.CompletionItem(
+              name,
+              vscode.CompletionItemKind.Variable
+            );
+            cItem.documentation = new vscode.MarkdownString(
+              `**Environment Variable**\n\n _File: ${file}_\n\n${name}=${value}`
+            );
+            return cItem;
+          })
+        );
+      } else {
+        resolve([]);
+      }
+    });
+  }
+
+  getEnvFilesInDir(dir: string, envVariables: EnvVarItem[]) {
+    let files = fs.readdirSync(dir);
+    for (let i in files) {
+      let name = dir + "/" + files[i];
+      if (!name.includes("node_modules") && fs.statSync(name).isDirectory()) {
+        this.getEnvFilesInDir(name, envVariables);
+      } else if (/\.env(\.local(\.[a-zA-Z0-9]+)*)?$/gi.test(files[i])) {
+        if (files[i].endsWith("js")) {
+          const exportedVariables = require(name);
+          for (const [key, value] of Object.entries(exportedVariables)) {
+            envVariables.push({
+              name: key,
+              value: value as string,
+              file: name,
+            });
+          }
+        } else {
+          let content = fs.readFileSync(name, "utf8");
+          let lines = content.split("\n");
+          for (let line of lines) {
+            let [key, value] = line.split("=");
+            if (key && value) {
+              envVariables.push({ name: key, value, file: name });
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+export function deactivate() {}
